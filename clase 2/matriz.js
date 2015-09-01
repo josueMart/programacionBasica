61739e4a84cd9455a434ddd51709c05479004611
JosueMart <josue.mart@live.com.mx>
2015-09-01 11:14:05 -0500

    Inicializacion del desarrollo del programa de campo minado

--- clase 2/matriz.js
+++ clase 2/matriz.js
@@ -1,1 +1,5 @@
-alert("prueba");
+// var menu=["Productos","Ventas","Contacto"];//declaracion de un vector
+// document.write(menu[2]);
+
+var dofa = [ ["Fuerza","Oportunidad"],["Debilidades","Amenazas"] ];
+document.write(dofa[1] [1]);
